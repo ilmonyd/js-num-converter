c651d8cb7766cddcd34ac93f7a8720cf28660ae2
Oleg Semenov <53193079+ilmonyd@users.noreply.github.com>
2021-04-29 17:55:57 +0200

    Fixed single digit conversion

--- converter.js
+++ converter.js
@@ -4,21 +4,21 @@ var minus = "[+["+dot+"+[+[]]+[+[]]+[+[]]+[+[]]+[+[]]+[+[]]+[++[[]][+[]]]]+[]][+
 
 function digitConversion(n) {
   var dig = "++[[]][+[]]", i = 0;
-  if (n == 0) {return "[+[]]"}
-  else if (n == ".") {return "["+dot+"]"}
-  else if (n == "-") {return "["+minus+"]"}
+  if (n == 0) {return "+[]"}
+  else if (n == ".") {return dot}
+  else if (n == "-") {return minus}
   else {
     for (i; i < n-1; i++) {
       dig = "++[" + dig + "][+[]]";
     }
   }
-  return "["+dig+"]";
+  return dig;
 }
 
 function convert(num) {
   var temp = [];
   for (const digit of num.toString()) {
-    temp.push(digitConversion(digit));
+    temp.push("["+digitConversion(digit)+"]");
   }
   return("+[" + temp.join("+") + "][+[]]");
 }
